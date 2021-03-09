fc96eefac40938542b7c4db9bb4363a29708c186
sammyCyclone <80020572+sammyCyclone@users.noreply.github.com>
2021-03-09 12:31:31 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 input.onButtonPressed(Button.A, function () {
     music.playMelody("E B C5 A B G A F ", 120)
+    basic.showIcon(IconNames.Heart)
 })
+basic.showString("Hello!")
 basic.forever(function () {
 	
 })
